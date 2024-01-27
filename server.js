3a7f8497b44fedcc0865c169533110fbac05f250
unknown <damiangarciaabreu@gmail.com>
2024-01-26 22:05:52 -0300

    22.05

--- server.js
+++ server.js
@@ -11,9 +11,9 @@ app.use(express.json());
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
-  });
+});
 
 app.get('/', (req, res) => {
     res.end('Server 1 corriendo...')
